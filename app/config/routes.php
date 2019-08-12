02c97562af66cf06af7edb1229e11b37426f4018
Vitor Giovane <vitorgpalves@gmail.com>
2019-08-12 11:17:47 -0300

    feature: finaliza a aula "CRUD"

--- app/config/routes.php
+++ app/config/routes.php
@@ -4,4 +4,8 @@ $router->add("get", "/", function () {
   return "In home.";
 });
 
+$router->add("get", "/users", "\App\Controllers\UsersController::index");
 $router->add("get", "/users/(\d+)", "\App\Controllers\UsersController::show");
+$router->add("POST", "/users/(\d+)", "\App\Controllers\UsersController::store");
+$router->add("PUT", "/users/(\d+)", "\App\Controllers\UsersController::update");
+$router->add("DELETE", "/users/(\d+)", "\App\Controllers\UsersController::delete");
--- app/src/Controllers/UsersController.php
+++ app/src/Controllers/UsersController.php
@@ -6,10 +6,30 @@ use App\Models\User;
 
 class UsersController
 {
+  public function index($container, $request)
+  {
+    return "Index";
+  }
+
   public function show($container, $request)
   {
     $user = new User($container);
     $user->create(["name" => "Vitor"]);
     return $user->get($request->attributes->get(1));
+  }
+
+  public function store($container, $request)
+  {
+    return "Store";
+  }
+
+  public function update($container, $request)
+  {
+    return "Update";
+  }
+
+  public function destroy($container, $request)
+  {
+    return "Destroy";
   }
 }
